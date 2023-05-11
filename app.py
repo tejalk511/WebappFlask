ad756e4a1681e97eb974f43b2fd5803f4baf8db1
tejalk511 <117337800+tejalk511@users.noreply.github.com>
2023-05-11 17:01:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 
 #load pickle model
-model = pickle.load(open('model1.pkl'))
+model = pickle.load(open('model1.pkl','rb'))
 
 #create flask app
 app = Flask(__name__)
